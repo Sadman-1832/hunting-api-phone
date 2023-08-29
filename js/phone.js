511c19409b65217212d1743ae11b30cb9dab83c7
Sadman-1832 <sadmansakib999@gmail.com>
2023-08-29 15:17:39 +0600

    added search field in js

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async() =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async(searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones)
@@ -11,12 +11,16 @@ const displayPhones = phones =>{
 // console.log(phones)
 // step-1: get the place where the elements will stay
 const phoneContainer = document.getElementById('phone-container');
+
+// auto clearing previous search
+phoneContainer.textContent = '';
+
 phones.forEach(phone => {
     console.log(phone)
     
     //step-2: create a div
     const phoneCard = document.createElement('div');
-    phoneCard.classList =  `card w-96 bg-gray-100 shadow-xl`
+    phoneCard.classList =  `card w-96 bg-gray-100 p-6 shadow-xl`
     // step-3: set innerHTML
     phoneCard.innerHTML = ` <figure><img src="${phone.image}" alt="Shoes" /></figure>
     <div class="card-body">
@@ -30,6 +34,13 @@ phones.forEach(phone => {
        phoneContainer.appendChild(phoneCard);
 
 })
+}
+// handle search btn:
+const clickHandler = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
 }
 
 loadPhone();
